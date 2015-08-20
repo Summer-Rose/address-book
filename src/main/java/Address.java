7d72a95b457a38f3a3e1b72bebe3a4508903102b
Yelena Belikova <yelenabel@gmail.com>
2015-08-20 11:26:11 -0700

    create address class

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -1,3 +1,25 @@
 public class Address {
+  private String mStreet;
+  private String mCity;
+  private String mState;
+  private int mZip;
 
+  public Address(String street, String city, String state, int zip) {
+    mStreet = street;
+    mCity = city;
+    mState = state;
+    mZip = zip;
+  }
+  public String getStreet() {
+    return mStreet;
+  }
+  public String getCity() {
+    return mCity;
+  }
+  public String getState() {
+    return mState;
+  }
+  public int getZip() {
+    return mZip;
+  }
 }

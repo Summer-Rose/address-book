2a570764afbf5d9405a9b72f4eda503034b80596
Yelena Belikova <yelenabel@gmail.com>
2015-08-20 10:58:04 -0700

    create Birthday class with year, date, month

--- src/main/java/Birthday.java
+++ src/main/java/Birthday.java
@@ -1,3 +1,23 @@
 public class Birthday {
-  
+  private String mMonth;
+  private int mDay;
+  private int mYear;
+
+  public Birthday(String month, int day, int year) {
+    mMonth = month;
+    mDay = day;
+    mYear = year;
+  }
+
+  public String getMonth() {
+    return mMonth;
+  }
+
+  public int getDay() {
+    return mDay;
+  }
+
+  public int getYear() {
+    return mYear;
+  }
 }

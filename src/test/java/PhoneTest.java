d79e3a59fa7479dd349068b266047da2d75f45e0
Yelena Belikova <yelenabel@gmail.com>
2015-08-20 10:59:19 -0700

    pass test for returning correct string and instantiating class correctly

--- src/test/java/PhoneTest.java
+++ src/test/java/PhoneTest.java
@@ -2,9 +2,20 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class PhoneTest {
+
   @Test
   public void phone_checkThatInstantiatesCorrectly_true() {
     Phone newPhone = new Phone("space", "111-222-3334");
     assertEquals(true, newPhone instanceof Phone);
+  }
+  @Test
+  public void getNumber_checkThatReturnsCorrectNumber() {
+    Phone newPhone = new Phone("banana", "111-777-8008");
+    assertEquals("111-777-8008", newPhone.getNumber());
+  }
+    @Test
+  public void getNumber_checkThatReturnsCorrectType() {
+    Phone newPhone = new Phone("banana", "111-777-8008");
+    assertEquals("banana", newPhone.getType());
   }
 }

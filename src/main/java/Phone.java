91d36f110ef8e8cc32817c657780bbe30d3bcd8f
Yelena Belikova <yelenabel@gmail.com>
2015-08-20 10:30:34 -0700

    create Phone class that has only two private variable

--- src/main/java/Phone.java
+++ src/main/java/Phone.java
@@ -1,3 +1,15 @@
 public class Phone {
+    private String mType;
+    private String mNumber;
 
+    public Phone(String type, String number) {
+      mType = type;
+      mNumber = number;
+    }
+    public String getType() {
+      return mType;
+    }
+    public String getNumber() {
+      return mNumber;
+    }
 }

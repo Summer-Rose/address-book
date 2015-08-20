d141f162836d191b20099f044136336489c7ce59
Yelena Belikova <yelenabel@gmail.com>
2015-08-20 10:58:30 -0700

    create class with type and email inputs

--- src/main/java/Email.java
+++ src/main/java/Email.java
@@ -1,3 +1,17 @@
 public class Email {
-  
+  private String mType;
+  private String mEmail;
+
+  public Email(String type, String email) {
+    mType = type;
+    mEmail = email;
+  }
+
+  public String getType() {
+    return mType;
+  }
+
+  public String getEmail() {
+    return mEmail;
+  }
 }
